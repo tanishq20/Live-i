e6adc18758ee23a18cec7c132f2ffe2d8e5e751a
amishajais21 <66247959+amishajais21@users.noreply.github.com>
2021-07-30 13:07:09 +0530

    Add files via upload

---
+++ frontend/static/js/app.js
@@ -0,0 +1,33 @@
+// Init Github
+const github = new Github()
+// Init UI
+const ui = new UI()
+
+// Search input
+const searchUser = document.getElementById('searchUser')
+
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+  // Get input text
+  const userText = e.target.value
+
+  if (userText !== '') {
+    // Make http call
+    github.getUser(userText).then((data) => {
+      if (data.profile.message === 'Not Found') {
+        // Show alert
+        ui.showAlert('User not found', 'alert alert-danger')
+        // Clear profile
+        ui.clearProfile()
+      } else {
+        // Show profile
+        ui.showProfile(data.profile)
+        // Show repos
+        ui.showRepos(data.repo)
+      }
+    })
+  } else {
+    // Clear profile
+    ui.clearProfile()
+  }
+})
